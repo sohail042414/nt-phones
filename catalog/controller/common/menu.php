099315f32858dcef499f0f558b1149a2a0fe9c22
sohail042414 <sohail042414@gmail.com>
2020-07-29 23:08:17 +0500

    menu completed

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -12,7 +12,9 @@ class ControllerCommonMenu extends Controller {
 
 		$categories = $this->model_catalog_category->getCategories(0);
 
+	
 		foreach ($categories as $category) {
+
 			if ($category['top']) {
 				// Level 2
 				$children_data = array();
@@ -32,40 +34,128 @@ class ControllerCommonMenu extends Controller {
 				}
 
 				// Level 1
-				$data['categories'][] = array(
+				$cat_data = array(
 					'name'     => $category['name'],
 					'children' => $children_data,
 					'column'   => $category['column'] ? $category['column'] : 1,
 					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
 				);
+
+				$filter_data = array(
+					'filter_category_id' => $category['category_id'],
+					'start'              => 0,
+					'limit'              => 20,
+				);
+	
+				$results = $this->model_catalog_product->getProducts($filter_data);
+	
+				foreach ($results as $result) {
+	
+					$cat_data['products'][] = array(
+						'product_id'  => $result['product_id'],
+						//'thumb'       => $image,
+						'name'        => $result['name'],
+						//'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+						//'price'       => $price,
+						//'special'     => $special,
+						//'tax'         => $tax,
+						//'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
+						//'rating'      => $result['rating'],
+						'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'])
+					);
+
+				}
+
+				if( isset($cat_data['products']) && count($cat_data['products']) > 0){
+					$data['categories'][] = $cat_data;
+				}
+
+			}
+		}
+
+		$data['categories'] = array_slice($data['categories'],0,4,true);
+		
+		$this->load->model('service/repair');
+		$this->load->model('service/repair_rate');
+
+		$repair_categories = $this->model_service_repair->getCategories();		
+
+		foreach($repair_categories as $category){
+
+			$cat_data = array(
+				'name' => $category['name'],
+				'href' => $this->url->link('service/repair/category', 'path='.$category['category_id'])
+			);
+
+			$filter_data = array(
+				'filter_category_id' => $category['category_id'],
+				'filter_repair' => TRUE,			
+			);
+
+			$results = $this->model_catalog_product->getProducts($filter_data);
+	
+			foreach ($results as $result) {
+				
+				$rates = $this->model_service_repair_rate->getProductRates($result['product_id']);			
+
+				if(count($rates) > 0){
+
+					$cat_data['products'][] = array(
+						'product_id'  => $result['product_id'],
+						'name'        => $result['name'],
+						'href'        => $this->url->link('service/repair/product', 'product_id=' . $result['product_id'])
+					);
+				}
+
+			}
+			if( isset($cat_data['products']) && count($cat_data['products']) > 0){
+				$data['repair_categories'][] = $cat_data;
+			}
+		}
+
+		
+
+		$this->load->model('service/sell');
+		$this->load->model('service/sell_rate');
+
+		$sell_categories = $this->model_service_sell->getCategories();		
+
+		foreach($sell_categories as $category){
+
+			$cat_data = array(
+				'name' => $category['name'],
+				'href' => $this->url->link('service/repair/category', 'path='.$category['category_id'])
+			);
+
+			$filter_data = array(
+				'filter_category_id' => $category['category_id'],
+				'filter_repair' => TRUE,			
+			);
+
+			$results = $this->model_catalog_product->getProducts($filter_data);
+	
+			foreach ($results as $result) {
+				
+				$rates = $this->model_service_sell_rate->getProductRates($result['product_id']);			
+
+				if(count($rates) > 0){
+
+					$cat_data['products'][] = array(
+						'product_id'  => $result['product_id'],
+						'name'        => $result['name'],
+						'href'        => $this->url->link('service/sell/product', 'product_id=' . $result['product_id'])
+					);
+				}
+
+			}
+			if( isset($cat_data['products']) && count($cat_data['products']) > 0){
+				$data['sell_categories'][] = $cat_data;
 			}
 		}
 
 
-		$categories = $this->model_catalog_category->getCategories(0);
-		
-		$this->tree_html = '';
-	
-		foreach ($categories as $category) {
-			$this->tree_level = 0;
-			$this->spacer = '>';
-			
-			$filter_data = array(
-				'filter_category_id'  => $category['category_id'],
-				'filter_sub_category' => true
-			);
-	
-			$record =  array(
-				'category_id' => $category['category_id'],
-				'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-				'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
-			);
-	
-			$data['repair'][] = $record;
-			$record['children_data'] = $this->getChildren($category,$data); 
-	
-		}
-	
+
+
 		$data['buy_href'] = $this->url->link('product/category/all');
 		$data['sell_href'] = $this->url->link('service/sell');
 		$data['repair_href'] = $this->url->link('service/repair');
--- catalog/controller/service/repair.php
+++ catalog/controller/service/repair.php
@@ -51,58 +51,6 @@ class ControllerServiceRepair extends Controller {
 		
 	}
 
-	public function category22() {
-
-		$this->load->language('service/sell');
-
-		$data['breadcrumbs'] = array();
-
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/home')
-		);
-
-		$data['text_your_details'] = "Enter following details";
-
-		$this->load->model('service/repair');
-
-		$categories = $this->model_service_repair->getCategories();
-
-		$this->load->model('tool/image');
-
-		foreach($categories as $result){
-
-			if ($result['image']) {
-				$image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-			} else {
-				$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-			}
-
-			$data['description'] = html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8');
-
-			$data['categories'][] = array(
-				'name' => $result['name'],
-				'image' => $image,
-				'href' => ''
-			);
-		}
-
-		// echo '<pre>';
-		// print_r($data['categories']);
-		// exit; 
-
-		$this->load->model('catalog/category');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['column_right'] = $this->load->controller('common/column_right');
-		$data['content_top'] = $this->load->controller('common/content_top');
-		$data['content_bottom'] = $this->load->controller('common/content_bottom');
-		$data['footer'] = $this->load->controller('common/footer');
-		$data['header'] = $this->load->controller('common/header');
-
-		$this->response->setOutput($this->load->view('service/repair', $data));
-		
-	}
-
 
 
 	public function category() {
